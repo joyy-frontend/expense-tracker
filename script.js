9423e068b631c9ec2a22651b6479d2a241fdfdfa
rhkk7157 <rhkk7157@gmail.com>
2024-10-12 16:48:13 -0700

    add modal popup css

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ export function renderIncome() {
     monthData.income.forEach((income, index) => {
         // Create income element container
         const incomeElement = document.createElement('div');
-        incomeElement.classList.add('expence-container');  // Ensure class is consistent
+        incomeElement.classList.add('income-first-container');  // Ensure class is consistent
         incomeElement.innerHTML = `
             <div class="income-box">
                 <p class="income-title" data-index="${index}">${income.description}</p>
