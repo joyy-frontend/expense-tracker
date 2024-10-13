b6aa8736bb3251bffdff1c405a932ddb1d454b11
rhkk7157 <rhkk7157@gmail.com>
2024-10-13 14:21:01 -0700

    update edit for income section

--- income.js
+++ income.js
@@ -1,14 +1,51 @@
+
+
+
+
+
+
+
 import { currentMonth, monthsData, updateTotals, renderIncome, renderBudgetTracking } from './script.js';
 
 const btnIncomeNew = document.getElementById("btnIncomeNew");
 const addIncomeModal = document.getElementById('addIncomeModal');
 const saveIncomeBtn = document.getElementById('saveIncomeBtn');
 const errorMessage = document.getElementById('errorMessage');
+const salary = document.querySelector('.salary');
 const editIncomeModal = document.getElementById('editIncomeModal');
 const editIncomeBtn = document.getElementById('editIncomeBtn');
+
 const incomeArray = [];
 
 renderIncome();
+
+// function renderIncome() {
+//   salary.innerHTML = '';
+
+//   incomeArray.forEach((income, index) => {
+//     const incomeElement = document.createElement('div');
+//     incomeElement.classList.add('expence-container');
+//     incomeElement.innerHTML = `
+//         <div class="income-box">
+//           <p class="income-title" data-index="${index}">${income.title}</p>
+//           <p class="income-amount" data-index="${index}">$${income.amount}</p>
+//           <button class="income-del" id="delIncomeBtn" data-index="${index}">Delete</button>
+//         </div>
+//     `;
+//     salary.appendChild(incomeElement);
+//   })
+
+//   document.querySelectorAll('.salary p').forEach(p => {
+//     p.addEventListener('click', (e) => {
+//       const index = Number(e.target.dataset.index);
+//       IncomeHandleEdit(index);
+//     })
+//   });
+
+//   document.querySelectorAll('.income-del').forEach(button => {
+//     button.addEventListener('click', incomeHandleDelete);
+//   })
+// }
 
 btnIncomeNew.addEventListener('click', () => {
   incomeClearModalInputs(addIncomeModal);
@@ -22,42 +59,106 @@ function IncomeHandleEdit(index) {
   editIncomeBtn.setAttribute('data-index', index);
 }
 
+function incomeHandleDelete(e) {
+  const index = e.target.dataset.index;
+  incomeArray.splice(index, 1);  
+  renderIncome();
+}
 
 // Save edited income
 editIncomeBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const index = editIncomeBtn.getAttribute('data-index');
-    const monthData = monthsData[currentMonth];
+  e.preventDefault();
+  const index = editIncomeBtn.getAttribute('data-index'); 
+  const incomeTitleEdit = document.getElementById('incomeTitleEdit');
+  const incomeAmountEdit = document.getElementById('incomeAmountEdit');
+  const monthData = monthsData[currentMonth];
+  const incomeTitleEditValue = document.getElementById('incomeTitleEdit').value.trim();
+  const incomeAmountEditValue = document.getElementById('incomeAmountEdit').value.trim();
+  const errorMessageEdit = document.getElementById('errorMessageEdit');  
 
-    const incomeTitleEdit = document.getElementById('incomeTitleEdit').value.trim();
-    const incomeAmountEdit = document.getElementById('incomeAmountEdit').value.trim();
-    const errorMessageEdit = document.getElementById('errorMessageEdit');  
+  monthData.income[index].description = document.getElementById('incomeTitleEdit').value;
+  monthData.income[index].amount = document.getElementById('incomeAmountEdit').value;
 
-    // Update the income with edited values
-    monthData.income[index].description = document.getElementById('incomeTitleEdit').value;
-    monthData.income[index].amount = document.getElementById('incomeAmountEdit').value;
-
-    // Check if the fields are not empty
-    if (incomeTitleEdit === '' || incomeAmountEdit === '') {
-        errorMessageEdit.textContent = 'Please fill in both fields before saving.';  
-        errorMessageEdit.style.color = 'red'; 
-        errorMessageEdit.style.display = 'block';  
-        return;  
-    }
-
-    renderIncome();  // Re-render income after editing
-    editIncomeModal.style.display = 'none';
+  if (incomeTitleEditValue === '' || incomeAmountEditValue === '') {
+    errorMessageEdit.textContent = 'Please fill in both fields before saving.';  
+    errorMessageEdit.style.color = 'red'; 
+    errorMessageEdit.style.display = 'block';  
+    return;  
+  }
+  
+  renderIncome();  // Re-render income after editing
+  editIncomeModal.style.display = 'none';
+  handleIncomeAction(incomeTitleEdit, incomeAmountEdit, editIncomeModal, 'edit', index);
 });
 
-// Clear modal input fields
+
+
+
+//     editIncomeBtn 
+//     e.preventDefault();
+//     const index = editIncomeBtn.getAttribute('data-index');
+//     const monthData = monthsData[currentMonth];
+
+//     const incomeTitleEditValue = document.getElementById('incomeTitleEdit').value.trim();
+//     const incomeAmountEditValue = document.getElementById('incomeAmountEdit').value.trim();
+//     const errorMessageEdit = document.getElementById('errorMessageEdit');  
+
+    
+//     monthData.income[index].description = document.getElementById('incomeTitleEdit').value;
+//     monthData.income[index].amount = document.getElementById('incomeAmountEdit').value;
+
+
+//     if (incomeTitleEdit === '' || incomeAmountEdit === '') {
+//         errorMessageEdit.textContent = 'Please fill in both fields before saving.';  
+//         errorMessageEdit.style.color = 'red'; 
+//         errorMessageEdit.style.display = 'block';  
+//         return;  
+//     }
+
+//     renderIncome();  // Re-render income after editing
+//     editIncomeModal.style.display = 'none';
+// });
+
 function incomeClearModalInputs(modal) {
     const inputs = modal.querySelectorAll('input');
-    const errorMessage = modal.querySelector('#errorMessage');
-    errorMessage.innerHTML = '';
     inputs.forEach(input => {
         input.value = '';
     });
 }
+
+function handleIncomeAction(titleInput, amountInput, modal, status, index) {
+  const errorMessageEdit = document.getElementById('errorMessageEdit'); 
+
+  if (titleInput.value.trim() !== '' && amountInput.value.trim() !== '') {
+    errorMessageEdit.style.display = 'none'; 
+
+    if (status === 'edit') {
+      incomeArray[index].title = titleInput.value;
+      incomeArray[index].amount = amountInput.value;
+    } else {
+      incomeArray.push({ title: titleInput.value, amount: amountInput.value });
+    }
+
+    renderIncome(); 
+    modal.style.display = 'none'; 
+  } else {
+    if (status === 'edit') {
+      errorMessageEdit.textContent = 'Please check your input values.';
+      errorMessageEdit.style.display = 'block';
+    } else {
+      errorMessage.textContent = 'Please check your input values.';
+      errorMessage.style.display = 'block';
+    }
+  }
+}
+
+saveIncomeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const incomeTitleInput = document.getElementById('incomeTitleInput');
+  const incomeAmountInput = document.getElementById('incomeAmountInput');
+  handleIncomeAction(incomeTitleInput, incomeAmountInput, addIncomeModal, 'save');
+});
+
 
 // Show the add income modal
 btnIncomeNew.addEventListener('click', () => {
@@ -93,17 +194,4 @@ function addIncome(amount, description) {
     updateTotals();  // Update totals
     renderBudgetTracking();  // Recalculate and update budget tracking
 }
-  const incomeTitleInput = document.getElementById('incomeTitleInput');
-  const incomeAmountInput = document.getElementById('incomeAmountInput');
-  
-  if (incomeTitleInput.value.trim() !== '' && incomeAmountInput.value.trim() !== '') {
-    errorMessage.style.display = 'none';
-    incomeArray.push({ title: incomeTitleInput.value, amount: incomeAmountInput.value });
-    renderIncome();
-    addIncomeModal.style.display = 'none';
-  } else {
-    errorMessage.textContent = 'Please check your input values.';
-    errorMessage.style.display = 'block';
-  }
-
 
